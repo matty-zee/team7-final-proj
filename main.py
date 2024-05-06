07ca041500de38ba43508eb2ef7fd3b44a59be3d
yukthadeesan <ydeesan@ucsd.edu>
2024-05-06 00:39:51 -0700

    commit

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from json.tool import main
 from tkinter.tix import MAIN
 from typing import Final
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes 
+
 
 TOKEN: Final = '6635383068:AAFJituA5_o8o7L_ypkBuc0YgQlggetRee8'
 BOT_USERNAME: Final = '@facial_recognizer_bot'
